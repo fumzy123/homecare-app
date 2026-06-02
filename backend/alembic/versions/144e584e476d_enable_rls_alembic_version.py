ef54b3d13ce9d641c5f94764b3fb65c0b5f259b6
Fume Uvwie <25962091+fumzy123@users.noreply.github.com>
2026-06-02 20:24:20 -0230

    fix: remove unused sqlalchemy imports from alembic migrations

--- backend/alembic/versions/144e584e476d_enable_rls_alembic_version.py
+++ backend/alembic/versions/144e584e476d_enable_rls_alembic_version.py
@@ -8,7 +8,6 @@ Create Date: 2026-06-02 18:32:42.670982
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
--- backend/alembic/versions/b48566b9bca6_enable_rls.py
+++ backend/alembic/versions/b48566b9bca6_enable_rls.py
@@ -8,7 +8,6 @@ Create Date: 2026-06-02 18:11:25.129349
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
